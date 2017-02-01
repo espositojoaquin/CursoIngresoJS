7f1a7c13ab51f95e93d424666e5f19f4d9e61279
espositojoaquin <joako-esposito@hotmail.com.ar>
2017-02-01 11:58:22 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,17 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
-	
+
+{ 
+	// esto es un comentario de una linea 
+	/* comentario mas extenso*/
+
+ var nombre; //defino variable
+ nombre="Joaquin"; //cargo variable
+ nombre=prompt("ingrese su nombre","natalia natalia")
+ alert("nombre  "+nombre);
+var person = prompt(" ingresa tu nombre", "Natalia Natalia");
+
+
 }
 
