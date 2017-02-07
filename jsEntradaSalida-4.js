2163b17b2aed20bdc3527a8c642bbb37e20822e5
espositojoaquin <joako-esposito@hotmail.com.ar>
2017-02-07 12:07:46 -0300

    Add files via upload

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,12 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+*/
+function Mostar()
+{  // forma 1 value="hola"
+   var nombe;
+   nombre=prompt("por favor ingresa tu nombre");
+   document.getElementById('elNombre').value = nombre; // muestra un resultado en base al codigo o id que pongamos
+
+alert("usted se llama  " +nombre+" y tiene "+edad+" );
+
---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,24 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+var numerouno;
+var numerodos;
+var resultado;
+
+numerouno=document.getElementById('numeroUno').value;
+numerodos=document.getElementById('numeroDos').value;
+
+numerouno=parseInt(numerouno);
+numerodos=parseInt(numerodos);
+resultado=numerouno+numerodos;
+
+alert("la suma es "+resultado);
+
+}
+
+/*aler(parseint(numerouno)+parseint(numerodos)); 
+funciona pero corremos riesgo de olvidarnos parentesis
+
+                                              
